15e9e52800c35024764901eaedabead2d6d2212f
wp629813 <wp629813@126.com>
2018-09-14 15:11:32 +0800

    2018091415

--- app.js
+++ app.js
@@ -1,47 +1,134 @@
 //app.js
+var config = require('config.js')
 App({
   //启动时执行的初始化工作
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch: function() {
+    this.getSystemInfo();
+    this.userLogin();
+   // this.getUserInfoF();
  
- 
--
-    wx.setStorageSync('logs', logs)
- 
- 
-    // 登录登录登录H-ddfdfdffd
- 
+  },
+  globalData: {
+    userInfo: null,
+    systemInfo: null
+  },
+  //获取设备信息
+  getSystemInfo: function () {
+    wx.getSystemInfo({
+      success: res => {
+        this.globalData.systemInfo = res
+      },
+      fail: res => { },
+      complete: res => { },
+    })
+  },
+//先登录
+  userLogin: function(){
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
+        wx.showNavigationBarLoading()
+        wx.showToast({
+          title: 'Loading……',
+          duration: 3000,
+          icon: 'loading'
+        })
+        this.getUserInfoF();
       }
     })
   },
  
-  globalData: {
-    userInfo: null
+  /** */
+  getUserInfoF: function() {
+    var that = this;
+    wx.getSetting({
+      success: (res) => {
+          /*
+        * res.authSetting = {
+        *   "scope.userInfo": true,
+        *   "scope.userLocation": true
+        * }
+        */
+
+        wx.getUserInfo({
+          success: res => {
+            wx.hideToast()
+            wx.hideNavigationBarLoading();
+            this.globalData.userInfo = res.userInfo
+            /**一开始同意授权 */
+            if (this.userInfoReadyCallback) {
+              this.userInfoReadyCallback(res)
+            }
+          },
+          fail(err) {
+            wx.hideToast()
+            wx.hideNavigationBarLoading();
+
+            wx.showModal({
+              title: '警告',
+              cancelText: '不授权',
+              confirmText: '授权',
+              confirmColor: '#37C31A',
+              content: '若不授权微信登录，则无法正常使用wiigie的更多服务；',
+
+              success: function(res) {
+                if (res.confirm) {
+                  /**用户点击确定 */
+
+                  wx.openSetting({
+                    success: (res) => {
+                      if (res.authSetting['scope.userInfo']) {
+                        wx.getUserInfo({
+                          success: res => {
+                            that.globalData.userInfo = res.userInfo
+                            /**再次同意授权*/
+                            // console.info("再次同意授权" + res.userInfo.nickName);
+                            if (that.userInfoReadyCallback) {
+                              that.userInfoReadyCallback(res)
+                            } 
+                          }
+                        })
+                      } else {
+                        /**再次不允许*/
+                        // console.info("再次不允许");
+                        wx.redirectTo({
+
+                          url: "/page/component/pages/pageauth/pageauth"
+                        })
+                      }
+                    }
+                  });
+                } else if (res.cancel) {
+                  console.log('弹出框用户点击取消')
+                  wx.redirectTo({
+                    url: 'home',
+                  })
+
+                }
+              }
+            })
+
+          }
+
+        })
+
+      }
+
+    })
+
+
   }
+
+
+  // cancel: function () {
+  //   this.setData({
+
+  //     'modal.modalhidden': true,
+  //   });
+  // },
+  // confirm: function () {
+  //   this.setData({
+  //     'modal.modalhidden': true,
+  //   });
+
+  // }
 })
--- pages/pagehome/pagehome.js
+++ pages/pagehome/pagehome.js
@@ -1,24 +1,33 @@
 // pages/pagehome/pagehome.js
 const app = getApp()
 
+var config = require('../../config.js')
+
 Page({
   data: {
     motto: 'Hello World',
+    // modal: {
+    //   modalhidden: true,
+    //   modaltitle: '',
+    //   modalnocancel: true,
+    //   modalinfo: 'sdfsf',
+    // },
+
     defaultInfo: {
-      searchHolder:'检索好友昵称或活动主题、描述',
+      searchHolder: '检索好友昵称或活动主题、描述',
       askText: '通知',
       askIcon: '/image/ask_0.png',
       // swiper属性
       swiperImgUrls: ['/image/home_swiper_1.jpg', '/image/home_swiper_2.jpg'],
-      swiperIndicatorDots:true,  //是否显示指示点   
-      swiperAutoplay:true,//是否自动切换
-      swiperInterval: 2000,//自动切换时间间隔
-      swiperDuration: 500,//duration 滑动动画时长
+      swiperIndicatorDots: true, //是否显示指示点   
+      swiperAutoplay: true, //是否自动切换
+      swiperInterval: 2000, //自动切换时间间隔
+      swiperDuration: 500, //duration 滑动动画时长
       //home-3
       home3Figure1: '/image/figure_1.png',
       home3Text: ['好友互荐', '正品保证', '无忧退货', '全场包邮'],
-       //home-4
-      home4Figure1:['',''],
+      //home-4
+      home4Figure1: ['', ''],
       home4Figure2: ['/image/figure_2.png', '/image/figure_3.png'],
       home4Text1: ['人以群分', '物以类聚'],
       home4Text2: ['分众崛起:互荐+优享', '消费升级:精致+别样'],
@@ -31,6 +40,7 @@ Page({
     userInfo: {},
 
     hasUserInfo: false,
+    // systemBaseInfo: {},
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
@@ -41,10 +51,12 @@ Page({
     })
   },
   onLoad: function() {
+
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: true,
+      
       })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -52,7 +64,8 @@ Page({
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: true,
+          
         })
       }
     } else {
@@ -62,14 +75,24 @@ Page({
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: true,
+ 
+
           })
         }
       })
     }
+
+
   },
+  onShow: function() {
+
+
+    }
+
+    ,
   getUserInfo: function(e) {
-    console.log(e)
+    // console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
@@ -77,12 +100,63 @@ Page({
     })
   },
 
-  homeSerarch:function(){
+  //检索
+  homeSerarch: function() {
 
-    wx.navigateTo({ 
+    wx.navigateTo({
       url: "/page/component/pages/pagewait/pagewait"
     })
 
-  }
+  },
+  //通知
+  homeNotice: function() {
+
+    wx.navigateTo({
+      url: "/page/component/pages/pagewait/pagewait"
+    })
+
+  },
+  //人以群分
+  homePerson: function() {
+
+    wx.navigateTo({
+      url: "/page/component/pages/pagewait/pagewait"
+    })
+
+  },
+  //物以类聚
+  homeGoods: function() {
+
+    wx.navigateTo({
+      url: "/page/component/pages/pagewait/pagewait"
+    })
+
+  },
+  //获取好友动态
+  getFriendsActive: function() {
+
+    wx.request({
+      url: config.requestUrl, //仅为示例，并非真实的接口地址
+      data: {
+        code_: 'x_getHome4',
+        homepageid: 'homepage_4',
+        userid: '',
+        endRow: '0',
+        itemsPerPage: '10',
+        windowWidth: app.globalData.systemInfo.windowWidth
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function(res) {
+        console.log(res.data)
+      }
+    })
+
+  },
+  //好友动态-详情
+
+  //人气推荐-详情
+
 
 })
