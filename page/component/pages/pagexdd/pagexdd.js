8efdaeb67b8f8a559ab0c92e2b3b2787e9378536
wp629813 <wp629813@126.com>
2018-09-17 17:46:14 +0800

    2018091717

--- page/component/pages/pagexdd/pagexdd.js
+++ page/component/pages/pagexdd/pagexdd.js
@@ -1,11 +1,34 @@
 // page/component/pages/pagexdd/pagexdd.js
+
+var config = require('../../../../config.js')
+
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    requirementInfo:{},
 
+    swiperArea:{
+      swiperImgUrls: ['/image/home_swiper_1.jpg', '/image/home_swiper_2.jpg'],
+      swiperIndicatorDots: true, //是否显示指示点   
+      swiperAutoplay: true, //是否自动切换
+      swiperInterval: 2000, //自动切换时间间隔
+      swiperDuration: 500, //duration 滑动动画时长
+    },
+
+
+    fixedBottom:{
+      oneGridWidth:0,
+      twoGridWidth:0,
+      gridNums: 8,//下单 送礼 推荐给 占用两个 首页或客服的 空间
+
+      xdClassName:'bottom-xd',
+      slClassName: 'bottom-sl',
+      tjClassName: 'bottom-tj'
+    }
   },
 
   /**
@@ -20,6 +43,16 @@ Page({
    */
   onReady: function () {
 
+
+    var windowWidth= app.globalData.systemInfo.windowWidth
+
+    var ongGridWidth = windowWidth / this.data.fixedBottom.gridNums
+
+    this.setData({
+
+      oneGridWidth: ongGridWidth+"px" ,
+      twoGridWidth: (ongGridWidth * 2) + "px" 
+    })
   },
 
   /**
@@ -61,6 +94,13 @@ Page({
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
+
+  },
+
+  /**获取详情信息 */
+
+  getRequirementInfo:function(){
+
 
   }
 })
