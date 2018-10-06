7f79d28e1d7d1d8817f9c0699533882e58fa8909
wp629813 <wp629813@126.com>
2018-10-06 22:11:09 +0800

    2018100622

--- page/component/pages/pagegift/giftreceivesucc/giftreceivesucc.js
+++ page/component/pages/pagegift/giftreceivesucc/giftreceivesucc.js
@@ -1,5 +1,7 @@
 // page/component/pages/pagegift/giftreceivesucc/giftreceivesucc.js
 var config = require('../../../../../config.js');
+var rCommon = require('../../../../../utils/rCommon.js');
+var rRequest = require('../../../../../utils/rRequest.js');
 var app = getApp();
 Page({
 
@@ -8,11 +10,14 @@ Page({
    */
   data: {
     giftInfo: {
+      giftRecordId: '',
       process: '1',
-      coverImage: config.imageUrl + "/wiigie/background/gift/give_gift_result_23.png", //展示的图片路径
-      giftSkuImage: '/image/goods-test.jpg'
+      giftStatusImage: config.imageUrl + "/wiigie/background/gift/give_gift_result_23.png", //展示的图片路径
+    
+      recordInfo:{}
     },
-
+   /**提示信息 */
+    configMsgInfo: {},
 
     /**tabbar */
     pageScrollView: {
@@ -28,9 +33,32 @@ Page({
   onLoad: function(options) {
     /****调用函数设置tabbar及页面*****/
     app.editTabBar();
-   
     /****调用函数设置tabbar及页面*****/
 
+    // window.location.replace("/wiigie/give/giveGiftResult?u=" + userid + "&gr=" + giftRecordId + "&t=" + operatortype);
+    var giftRecordId = options.gr;
+    var t = options.t;
+    
+    var process = t;
+    var giftStatusImage = '';
+    if (t == '1') {
+
+      giftStatusImage = config.imageUrl + "/wiigie/background/gift/give_gift_result_23.png"; //展示的图片路径
+    }
+    if (t == '2') {
+      giftStatusImage = config.imageUrl + "/wiigie/background/gift/give_gift_result_24.png"; //展示的图片路径
+    }
+    if (t == '3') {
+      giftStatusImage = config.imageUrl + "/wiigie/background/gift/give_gift_result_21.png"; //展示的图片路径
+    }
+
+    this.setData({
+      'giftInfo.giftRecordId': giftRecordId,
+      'giftInfo.process': process,
+      'giftInfo.giftStatusImage': giftStatusImage,
+    })
+    this. getGiveGiftRecordInfo()
+    
   },
 
   /**
@@ -80,5 +108,71 @@ Page({
    */
   onShareAppMessage: function() {
 
-  }
+  },
+  /** */
+  getGiveGiftRecordInfo:function(){
+
+    var that = this
+    var giftRecordId = '1537842834692515';
+  
+    var url = config.requestUrl
+    var data = {
+      code_: 'x_getGiveGiftRecord',
+      giftRecordId: giftRecordId,
+  
+    }
+    rRequest.doRequest(url, data, that, function (rdata) {
+
+      if (rdata.info) {
+
+        that.setData({
+          'giftInfo.recordInfo': rdata.info
+        
+        })
+ 
+
+        that.getConfigMsgInfo()
+      }
+    })
+
+  },
+
+  /**获取配置描述 */
+  getConfigMsgInfo: function () {
+    var that = this;
+    var url = config.requestUrl;
+
+    var fUserNickname = that.data.giftInfo.recordInfo.from_person_nickname;
+    var values = [{
+      code: 'RESULT_23',
+      replace: [{ regexp: 'nickname', replacement: fUserNickname }]
+    }, {
+        code: 'RESULT_24',
+        replace: [{ regexp: 'nickname', replacement: fUserNickname }]
+      }, {
+        code: 'RESULT_21',
+        replace: [{ regexp: 'nickname', replacement: fUserNickname }]
+      }
+
+    ];
+
+
+    var data = {
+      code_: 'x_getConfigMsgInfo',
+      /**[{code:xxxx,replace:[{regexp:xxx,replacement:xxxx},{}]},{}] */
+      values: values
+    }
+    rCommon.configMsgInfo.getConfigMsg(url, data, that, function (rdata) {
+      if (rdata.info) {
+
+        that.setData({
+          configMsgInfo: rdata.info,
+
+        })
+
+      }
+
+    });
+
+  },
 })
