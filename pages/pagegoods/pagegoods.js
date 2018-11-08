a51c5a80320a332f8e8bb257b3ef1fba6aee5400
wp629813 <wp629813@126.com>
2018-11-08 22:39:54 +0800

    2018110822

--- pages/pagegoods/pagegoods.js
+++ pages/pagegoods/pagegoods.js
@@ -13,7 +13,7 @@ Page({
     loginInfo: app.globalData.loginInfo,
     promotiontag: '',
 
-    
+
     promotiontagArry: [],
 
     promotiontagNum: 5, //允许横向显示的个数
@@ -21,26 +21,37 @@ Page({
     promotiontagCount: 0, //已最小单元划分时，最小单元的个数
     promotiontagWidth: 0, //已最小单元划分时，最小单元的宽度
 
-    goodsArry:[],
+    goodsArry: [],
+    goodsCount: 0,
+    goodsSelected: false,
+    goodsendRow: 0,
+    itemsPerPage: 10,
 
     windowWidth: app.globalData.systemInfo.windowWidth,
     screenWidth: app.globalData.systemInfo.screenWidth,
     windowHeight: app.globalData.systemInfo.windowHeight,
-    scrollViewHeight:0,
+    scrollViewHeight: 0,
     imageSize: app.globalData.systemInfo.screenWidth,
 
-      /**用户信息 */
+    /**用户信息 */
     userInfo: {},
     //hasUserInfo: false,
     userIData: false,
     userWxInfo: {},
 
+
+
+    //是否下拉刷新
+    isPullDownRefresh: false,
+    //是否上拉更多
+    isReachBottom: false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
+
     if (app.globalData.userWxInfo) {
       this.setData({
         userWxInfo: app.globalData.userWxInfo,
@@ -58,11 +69,13 @@ Page({
    */
   onReady: function() {
 
-    var viewTagHeight = (this.data.screenWidth/750) *100
+    var windowWidth = this.data.screenWidth
+    var percent = windowWidth / 750
+    var viewTagHeight = 100 * percent
 
     this.setData({
 
-      scrollViewHeight: (this.data.windowHeight - viewTagHeight-5)+"px"
+      scrollViewHeight: (this.data.windowHeight - viewTagHeight - 5) + "px"
     })
     wx.hideShareMenu();
   },
@@ -93,15 +106,42 @@ Page({
    */
   onPullDownRefresh: function() {
 
+    this.setData({
+      goodsendRow: '0',
+      goodsCount: '0',
+      isPullDownRefresh: true
+    })
+    this.getGoods();
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
+    // this.setData({
+    //   isReachBottom: true
+    // })
+    // this.getGoods()
+  },
+  scroll: function(e) {
 
   },
 
+  upper: function(e) {
+    // this.setData({
+    //   goodsendRow: '0',
+    //   goodsCount: '0',
+    //   isPullDownRefresh: true
+    // })
+    // this.getGoods();
+  },
+  lower: function(e) {
+    this.setData({
+      isReachBottom: true
+    })
+    this.getGoods()
+  },
   /**
    * 用户点击右上角分享
    */
@@ -110,13 +150,13 @@ Page({
   },
 
 
-  goodShowDetail: function (event) {
+  goodShowDetail: function(event) {
     var that = this;
 
     var upmarkid = event.currentTarget.dataset.upmarkid;
     var requirementid = event.currentTarget.dataset.requir;
     var userid = that.data.userInfo.id;
-    pagegood.pageGood.showDetail(upmarkid, requirementid );
+    pagegood.pageGood.showDetail(upmarkid, requirementid);
 
 
   },
@@ -153,58 +193,107 @@ Page({
         if (wCount == 0) {
           lwidth = w / 1;
         }
-     
 
-          this.setData({
-            promotiontagArry: res.data.infolist,
 
-            promotiontagCount: wCount,
-            promotiontagWidth: lwidth+"px" 
-          })
+        this.setData({
+          promotiontagArry: res.data.infolist,
+
+          promotiontagCount: wCount,
+          promotiontagWidth: lwidth + "px"
+        })
 
       }
     })
   },
- 
-  selectTag:function(event){
- 
-    var tagCode = event.currentTarget.dataset.tagcode;
 
-    this.setData({
-      promotiontag: tagCode,
-    })
+  selectTag: function(event) {
 
-    this.getGoods();
-  }
+      var tagCode = event.currentTarget.dataset.tagcode;
 
-  ,
+      this.setData({
+        promotiontag: tagCode,
+      })
+
+      this.getGoods();
+    }
+
+    ,
 
   getGoods: function() {
 
 
     var that = this;
+    var isPullDownRefresh = that.data.isPullDownRefresh;
+    var isReachBottom = that.data.isReachBottom;
+
+    var endRow = that.data.goodsendRow
+    var itemsPerPage = that.data.itemsPerPage
+    var goodsCount = that.data.goodsCount
+
+    if (isReachBottom && goodsCount == endRow) {
+
+      that.setData({
+        isReachBottom: false,
+      })
+      wx.showToast({
+        title: '没有更多了',
+        icon: 'none',
+        duration: 1500,
+        success: function() {}
+      })
+      return
+    }
+
     var url = config.requestUrl;
-    var usreId = '1527671283933540'
+    var usreId = that.data.userInfo.id
     var tag = this.data.promotiontag;
+
+    wx.showLoading({
+      title: '加载中...',
+      mask: true,
+    })
+
     var data = {
       code_: 'x_getGoods',
       goodspageid: 'goodspage_1',
       userid: usreId,
-      endRow: '0',
-      itemsPerPage: '10',
+      endRow: endRow,
+      itemsPerPage: itemsPerPage,
       promotiontag: tag,
     }
-    rRequest.doRequest(url, data, that, function (rdata) {
-      console.log("-----------------")
+    rRequest.doRequest(url, data, that, function(rdata) {
+
       if (rdata.infolist) {
+
+        var goodsArry = [];
+
+        if (isPullDownRefresh) {
+          goodsArry = [];
+
+          wx.stopPullDownRefresh();
+        }
+        if (isReachBottom) {
+          goodsArry = that.data.goodsArry;
+        }
+        var goodsArryNew = [...goodsArry, ...rdata.infolist]
+
 
         that.setData({
 
-          goodsArry: rdata.infolist
+          goodsArry: goodsArryNew,
+
+          goodsSelected: true,
+          goodsCount: rdata.infocounts,
+          goodsendRow: rdata.endRow,
+          isPullDownRefresh: false,
+          isReachBottom: false,
         })
       }
+
+
+      wx.hideLoading();
     })
 
-   }
- 
+  }
+
 })
